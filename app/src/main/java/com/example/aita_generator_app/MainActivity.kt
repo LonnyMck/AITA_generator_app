8636b400657a857d08cbc2fa42997e12a068d724
Lonny McKiernan <lonnymckiernan@gmail.com>
2023-09-03 14:45:24 +0100

    general updates

--- app/src/main/java/com/example/aita_generator_app/MainActivity.kt
+++ app/src/main/java/com/example/aita_generator_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.aita_generator_app
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,13 +8,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
@@ -34,8 +39,8 @@ class MainActivity : ComponentActivity() {
 
                     val vm = ViewModelProvider(this)[SampleViewModel::class.java]
 
-                    Greeting(
-                        vm.mobGen,
+                    AITAmain(
+                        vm.starter,  vm.opinion, vm.commenter, vm.location, vm.involves,
                         clickHandle1 = {vm.handleAction( SampleViewModel.Action.ButtonOne)},
                         clickHandle2 = {vm.handleAction( SampleViewModel.Action.ButtonTwo)}
                         )
@@ -50,35 +55,86 @@ data class SampleThing(val fish: Int, val cookies:String = "default")
 
 @Composable
 fun RandoClick( clickHandle:()->Unit, displayName: String = "default"){
-Button( onClick = clickHandle, modifier = Modifier.padding(10.dp) ){
-    Text(displayName)
+Button( onClick = clickHandle,
+    modifier = Modifier
+        .fillMaxWidth(),
+
+    //colors = ButtonDefaults.buttonColors(containerColor = Color.BLACK),
+    shape = CutCornerShape(2),
+    //colors =  ButtonDefaults.textButtonColors(Color.BLACK)
+
+
+){
+
+    Text(displayName,
+        fontWeight = FontWeight.Bold,
+
+    )
 }
 
 
 }
+
 
 @Composable
-fun Greeting(name: String, clickHandle1:()->Unit, clickHandle2:()->Unit, modifier: Modifier = Modifier) {
+fun AITAmain(starter: String, opinion: String, commenter: String, location:String, involves: String, clickHandle1:()->Unit, clickHandle2:()->Unit, modifier: Modifier = Modifier) {
 
-    Column() {
+    Column( modifier = Modifier.padding(40.dp) ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+
+
+        ){
+            RandoClick(clickHandle1, displayName = "AITA")
+        }
+
+        Column(
+            Modifier.weight(1f)
+        ){
         Text(
-
-            //text = "Hello ${name}, ${sample.cookies}!",
-            text = "I don't know how to feel about Minecraft, I'm so scared of the $name",
+            text = "$starter...",
             modifier = modifier
         )
-        Row(horizontalArrangement = Arrangement.Center){
-            RandoClick(clickHandle1, displayName = "Scared")
-            RandoClick(clickHandle2, displayName = "Cute")
+
+        Text(
+            text = "Location: $location",
+            modifier = modifier
+        )
+
+        Text(
+            text = "Involves: $involves",
+            modifier = modifier
+        )
         }
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+
+
+        ) {
+            RandoClick(clickHandle2, displayName = "Comment")
+        }
+        Column(Modifier.weight(2f)){
+        Text(
+            text = "$commenter thinks $opinion because..."
+        )
+        }
+
     }
 }
+
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     AITA_generator_appTheme {
         //val sample2 = SampleThing(5, "sweet")
-        Greeting("Android", {}, {} )
+        AITAmain("I, 28F...",  "YTA", "A baby","funeral", "banana bread", {},{} )
     }
 }
--- app/src/main/java/com/example/aita_generator_app/SampleViewModel.kt
+++ app/src/main/java/com/example/aita_generator_app/SampleViewModel.kt
@@ -10,8 +10,14 @@ import kotlin.random.Random.Default.nextInt
 class SampleViewModel (
     app: Application
 ): AndroidViewModel( app ){
-    var mobGen by mutableStateOf("n/a")
-
+    var age by mutableStateOf("AGE")
+    var gender by mutableStateOf("GENDER")
+    var relation by mutableStateOf("RELATION")
+    var starter by mutableStateOf("I (___)")
+    var location by mutableStateOf("___")
+    var involves by mutableStateOf("___")
+    var opinion by mutableStateOf("___")
+    var commenter by mutableStateOf("___")
     fun handleAction( action: Action ){
         when(action){
             Action.ButtonOne -> randoClick1()
@@ -20,13 +26,37 @@ class SampleViewModel (
     }
 
     fun randoClick1(){
-        val option = nextInt(0, LISTONE.size)
-        mobGen = LISTONE[option]
+
+        val optionage = nextInt(0,LISTAGE.size)
+        age = LISTAGE[optionage].toString()
+
+        val optiongender = nextInt(0, LISTGENDER.size)
+        gender = LISTGENDER[optiongender]
+
+        val optionrelation = nextInt(0, LISTRELATION.size)
+        relation = LISTRELATION[optionrelation]
+
+        val fiftyfifty = nextInt(0,2)
+
+        if (fiftyfifty == 0){
+            starter = "I ($age$gender)"
+        }else{
+            starter = "My ($age$gender) $relation"
+        }
+
+        val optioninvolves = nextInt(0, LISTINVOLVES.size)
+        involves = LISTINVOLVES[optioninvolves]
+
+        val optionlocation = nextInt(0, LISTLOCATION.size)
+        location = LISTLOCATION[optionlocation]
     }
 
     fun randoClick2(){
-        val option = nextInt(0, LISTTWO.size)
-        mobGen = LISTTWO[option]
+        val optionopinion = nextInt(0, LISTOPINION.size)
+        opinion = LISTOPINION[optionopinion]
+
+        val optioncommenter = nextInt(0, LISTCOMMENTER.size)
+        commenter = LISTCOMMENTER[optioncommenter]
     }
 
 
@@ -37,7 +67,14 @@ class SampleViewModel (
     }
 
     companion object {
-        val LISTONE = listOf<String>("zombies", "skeletons", "witches", "phantoms", "creepers")
-        val LISTTWO = listOf<String>("villagers", "dolphins", "sheep", "foxes", "endermen")
+        val LISTAGE = listOf<Int>(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70)
+        val LISTGENDER = listOf<String>("M","M","M","M","M","M","M","M","M","M","M","M","M","M","M","M","M","M","M","M","NB","F","F","F","F","F","F","F","F","F","F","F","F","F","F","F","F","F","F","F","F",)
+        val LISTRELATION = listOf<String>("Parent","Mom", "Dad", "Grandma", "Grandpappy","Papa","Grandparent","Child","Son","Daughter","Cousin","Sibling", "Long-term Long-Distance Low-Commitment Casual Girlfriend","Partner","Girlfriend","Boyfriend","Spouse","Co-worker","Dog","Cat","Pizza Delivery Guy","FWB","Mother-in-Law","Hairdresser")
+
+        val LISTLOCATION = listOf<String>("Wedding", "Papa's Pizzeria","The Morgue","Buckingham Palace", "The White House", "A Tropical Island", "Kyrgzstan", "Quebec","The middle of the dessert, no civilization in site", "Mickey Mouse Clubhouse","AirBNB", "Family Christmas Event", "Home", "Vacation", "Bachelor Party", "Bachelorette Party", "FWB's house","Cruiseship","Middle of the woods", "Sex Dungeon", "Relative's House", "Orgy", "Office", "Frat Party", "Funeral")
+        val LISTINVOLVES = listOf<String>("Grandma's Secret Recipe","Fire","Rival Households","Immortality", "A six-fingered hand", "Petty Family Drama", "A magical potion that turns you gay at midnight","DIY conversion therapy (you couldn't afford actual conversion therapy)","A swarm of bees", "Strippers", "Sex", "Divorce", "Infidelity", "Faking Sick", "The US government", "pseudo-incest","a bad haircut", "Funeral Arrangements")
+
+        val LISTOPINION = listOf<String>("YTA","YTA","YTA","YTA","YTA","NTA","NTA","NTA","NTA","ESH","NAH","INFO")
+        val LISTCOMMENTER = listOf<String>("Santa", "A reddit user who tries to humblebrag about himself whenever he can", " A tiktok user who didn't listen and is trying to get as many likes as possible in the comment section", "A reddit user trying to break up as many couples as he can","OP's favourite author", "OP's therapist","An alien trying to learn about humans and their customs from Reddit","A time traveler trying to subtly find out what year it is","a concerned mother", "a firefighter", "your 9-month old son", "the president","OP's grandmother", "a femcel", "a normal well-adjusted individual", "OP's one-night-stand", "a tiktok commenter")
     }
 }
